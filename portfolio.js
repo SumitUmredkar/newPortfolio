a83ac9c7a019249568bd9c646ff9d26f117c9af8
NewProjet <sumitumryt07@gmail.com>
2024-07-14 20:24:52 +0530

    updated

--- portfolio.js
+++ portfolio.js
@@ -32,41 +32,41 @@ app
       .to(laptop.position, { x: 400, y: -2700 }, 0);
   });
 
-// const heading = document.getElementById("heading");
+const heading = document.getElementById("heading");
 
-// document.addEventListener("mousemove", (e) => {
-//   const { clientX: x, clientY: y } = e;
-//   const {
-//     offsetWidth: width,
-//     offsetHeight: height,
-//     offsetLeft: left,
-//     offsetTop: top,
-//   } = heading;
-//   const centerX = left + width / 2;
-//   const centerY = top + height / 2;
-//   const deltaX = x - centerX;
-//   const deltaY = y - centerY;
+document.addEventListener("mousemove", (e) => {
+  const { clientX: x, clientY: y } = e;
+  const {
+    offsetWidth: width,
+    offsetHeight: height,
+    offsetLeft: left,
+    offsetTop: top,
+  } = heading;
+  const centerX = left + width / 2;
+  const centerY = top + height / 2;
+  const deltaX = x - centerX;
+  const deltaY = y - centerY;
 
-//   const maxShadowOffset = 20; // maximum shadow offset in px
-//   const shadowX = (deltaX / width) * -60;
-//   const shadowY = (deltaY / height) * -15;
+  const maxShadowOffset = 20; // maximum shadow offset in px
+  const shadowX = (deltaX / width) * -60;
+  const shadowY = (deltaY / height) * -15;
 
-//   heading.style.textShadow = `${shadowX}px ${shadowY}px 5px rgba(0, 0, 0, 0.3)`;
-// });
+  heading.style.textShadow = `${shadowX}px ${shadowY}px 5px rgba(0, 0, 0, 0.3)`;
+});
 
-// gsap.from("header", {
-//   left: 0,
-//   duration: 0.5,
-// });
+gsap.from("header", {
+  left: 0,
+  duration: 0.5,
+});
 
-// gsap.from("header>a", {
-//   x: -150,
-//   duration: 1.4,
-//   stagger: 0.2,
-// });
+gsap.from("header>a", {
+  x: -150,
+  duration: 1.4,
+  stagger: 0.2,
+});
 
-// gsap.from("#navigate a", {
-//   x: -150,
-//   duration: 1.4,
-//   stagger: 0.2,
-// });
+gsap.from("#navigate a", {
+  x: -150,
+  duration: 1.4,
+  stagger: 0.2,
+});
